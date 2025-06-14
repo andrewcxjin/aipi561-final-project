9956504db8a5ece6102090b7283e783e11eb589e
andrewcxjin <andrewcxjin@gmail.com>
2025-06-15 01:55:46 +0800

    changed app port

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from app.routes import routes
+import os
 
 app = Flask(__name__)
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
